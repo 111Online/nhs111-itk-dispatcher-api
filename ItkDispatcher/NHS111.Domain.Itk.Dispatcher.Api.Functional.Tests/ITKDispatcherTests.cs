c5a0196501ae15b9a2ea66490d0ce734d46774a8
patrick-hughes <patrick.hughes1@hscic.gov.uk>
2016-07-08 12:09:38 +0100

    Updated packages

--- ItkDispatcher/NHS111.Domain.Itk.Dispatcher.Api.Functional.Tests/ITKDispatcherTests.cs
+++ ItkDispatcher/NHS111.Domain.Itk.Dispatcher.Api.Functional.Tests/ITKDispatcherTests.cs
@@ -18,7 +18,7 @@ namespace NHS111.Business.Itk.Dispatcher.API.Functional.Tests
 
         //Test to show failure when Name field in service details not sent
         [Test]
-        public async void TestInvalidITKMessage_Fails()
+        public void TestInvalidITKMessage_Fails()
         {
             var endpoint = "SendItkMessage";
             var itkDisptatchRequest = ("{\"Authentication\":{\"UserName\":\"admn\",\"Password\":\"admnUat\"},\"PatientDetails\":{\"Forename\":\"TestForename\",\"Surname\":\"TestSurname\",\"Gender\":\"Male\",\"DateOfBirth\":\"11/11/1980\",\"ServiceAddressPostcode\":\"TS196TH\",\"TelephoneNumber\":\"02070 033002\",\"HomeAddress\":{\"PostalCode\":\"TS176TH\",\"StreetAddressLine1\":\"1 Test Lane\"},\"GpPractice\":{\"Name\":\"Test GP\",\"Telephone\":\"02380 666454\",\"ODS\":\"RHYAA\",\"Address\":{\"PostalCode\":\"GP3 6FF\",\"StreetAddressLine1\":\"1 GP Street\"}},\"ServiceDetails\":{\"Id\":\"158960\",\"OdsCode\":\"13524169111352416911\"}}");
@@ -26,19 +26,17 @@ namespace NHS111.Business.Itk.Dispatcher.API.Functional.Tests
 
             System.Net.ServicePointManager.Expect100Continue = false;
             var result =
-                await _restfulHelper.PostAsync(address, CreateHTTPRequest(itkDisptatchRequest));
+                _restfulHelper.PostAsync(address, CreateHTTPRequest(itkDisptatchRequest));
 
-            var resultContent = await result.Content.ReadAsStringAsync();
-
-            Assert.IsNotNull(result);
-            Assert.IsFalse(result.IsSuccessStatusCode);
-            Assert.AreEqual(result.StatusCode, HttpStatusCode.InternalServerError);
+            Assert.IsNotNull(result.Result);
+            Assert.IsFalse(result.Result.IsSuccessStatusCode);
+            Assert.AreEqual(result.Result.StatusCode, HttpStatusCode.InternalServerError);
             //these check the wrong fields are not returned
 
         }
                 //Test to show failure when invalid authentication is sent
         [Test]
-        public async void TestUnauthenticatedITKMessage_Fails()
+        public void TestUnauthenticatedITKMessage_Fails()
         {
             var endpoint = "SendItkMessage";
             var itkDisptatchRequest = ("{\"Authentication\":{\"UserName\":\"Test\",\"Password\":\"Test\"},\"PatientDetails\":{\"Forename\":\"TestForename\",\"Surname\":\"TestSurname\",\"Gender\":\"Male\",\"DateOfBirth\":\"11/11/1980\",\"ServiceAddressPostcode\":\"TS196TH\",\"TelephoneNumber\":\"02070 033002\",\"HomeAddress\":{\"PostalCode\":\"TS176TH\",\"StreetAddressLine1\":\"1 Test Lane\"},\"GpPractice\":{\"Name\":\"Test GP\",\"Telephone\":\"02380 666454\",\"ODS\":\"RHYAA\",\"Address\":{\"PostalCode\":\"GP3 6FF\",\"StreetAddressLine1\":\"1 GP Street\"}},\"ServiceDetails\":{\"Id\":\"158960\",\"OdsCode\":\"13524169111352416911\"}}");
@@ -46,13 +44,11 @@ namespace NHS111.Business.Itk.Dispatcher.API.Functional.Tests
 
             System.Net.ServicePointManager.Expect100Continue = false;
             var result =
-                await _restfulHelper.PostAsync(address, CreateHTTPRequest(itkDisptatchRequest));
+                _restfulHelper.PostAsync(address, CreateHTTPRequest(itkDisptatchRequest));
 
-            var resultContent = await result.Content.ReadAsStringAsync();
-
-            Assert.IsNotNull(result);
-            Assert.IsFalse(result.IsSuccessStatusCode);
-            Assert.AreEqual(result.StatusCode, HttpStatusCode.InternalServerError);
+            Assert.IsNotNull(result.Result);
+            Assert.IsFalse(result.Result.IsSuccessStatusCode);
+            Assert.AreEqual(result.Result.StatusCode, HttpStatusCode.InternalServerError);
 
         }
 
