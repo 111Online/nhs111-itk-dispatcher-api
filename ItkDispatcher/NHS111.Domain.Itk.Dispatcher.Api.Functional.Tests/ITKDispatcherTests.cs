7275ab1aa779eee3bf5a5508786a226903d1f1d2
Adam Naylor <adamnaylor@gmail.com>
2016-08-04 11:35:20 +0100

    Refactored itk password away.

--- ItkDispatcher/NHS111.Domain.Itk.Dispatcher.Api.Functional.Tests/ITKDispatcherTests.cs
+++ ItkDispatcher/NHS111.Domain.Itk.Dispatcher.Api.Functional.Tests/ITKDispatcherTests.cs
@@ -7,6 +7,8 @@ using NUnit.Framework;
 
 namespace NHS111.Business.Itk.Dispatcher.API.Functional.Tests
 {
+    using System.Configuration;
+
     [TestFixture]
     public class ItkDispatcherTests
     {
@@ -16,12 +18,20 @@ namespace NHS111.Business.Itk.Dispatcher.API.Functional.Tests
         
         private readonly RestfulHelper _restfulHelper = new RestfulHelper();
 
+        private static string Username {
+            get { return ConfigurationManager.AppSettings["itk_credential_user"]; }
+        }
+
+        private static string Password {
+            get { return ConfigurationManager.AppSettings["itk_credential_password"]; }
+        }
+
         //Test to show failure when Name field in service details not sent
         [Test]
         public void TestInvalidITKMessage_Fails()
         {
             var endpoint = "SendItkMessage";
-            var itkDisptatchRequest = ("{\"Authentication\":{\"UserName\":\"admn\",\"Password\":\"admnUat\"},\"PatientDetails\":{\"Forename\":\"TestForename\",\"Surname\":\"TestSurname\",\"Gender\":\"Male\",\"DateOfBirth\":\"11/11/1980\",\"ServiceAddressPostcode\":\"TS196TH\",\"TelephoneNumber\":\"02070 033002\",\"HomeAddress\":{\"PostalCode\":\"TS176TH\",\"StreetAddressLine1\":\"1 Test Lane\"},\"GpPractice\":{\"Name\":\"Test GP\",\"Telephone\":\"02380 666454\",\"ODS\":\"RHYAA\",\"Address\":{\"PostalCode\":\"GP3 6FF\",\"StreetAddressLine1\":\"1 GP Street\"}},\"ServiceDetails\":{\"Id\":\"158960\",\"OdsCode\":\"13524169111352416911\"}}");
+            var itkDisptatchRequest = string.Format("{{\"Authentication\":{{\"UserName\":\"{0}\",\"Password\":\"{1}\"}},\"PatientDetails\":{{\"Forename\":\"TestForename\",\"Surname\":\"TestSurname\",\"Gender\":\"Male\",\"DateOfBirth\":\"11/11/1980\",\"ServiceAddressPostcode\":\"TS196TH\",\"TelephoneNumber\":\"02070 033002\",\"HomeAddress\":{{\"PostalCode\":\"TS176TH\",\"StreetAddressLine1\":\"1 Test Lane\"}},\"GpPractice\":{{\"Name\":\"Test GP\",\"Telephone\":\"02380 666454\",\"ODS\":\"RHYAA\",\"Address\":{{\"PostalCode\":\"GP3 6FF\",\"StreetAddressLine1\":\"1 GP Street\"}}}},\"ServiceDetails\":{{\"Id\":\"158960\",\"OdsCode\":\"13524169111352416911\"}}}}", Username, Password);
             var address = String.Format(_domainITKApi + endpoint);
 
             System.Net.ServicePointManager.Expect100Continue = false;
