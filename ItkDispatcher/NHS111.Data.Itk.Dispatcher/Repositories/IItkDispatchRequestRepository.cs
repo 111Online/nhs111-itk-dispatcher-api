2258a4fc925648a76abb668828588fddf0f632a2
Adam Naylor <adamnaylor@gmail.com>
2020-03-04 11:59:47 +0000

    Downgraded projects to .NET Framework from Core

--- ItkDispatcher/NHS111.Data.Itk.Dispatcher/Repositories/IItkDispatchRequestRepository.cs
+++ ItkDispatcher/NHS111.Data.Itk.Dispatcher/Repositories/IItkDispatchRequestRepository.cs
@@ -1,9 +1,11 @@
 ﻿
-namespace NHS111.Data.Itk.Dispatcher.Repositories {
+namespace NHS111.Data.Itk.Dispatcher.Repositories
+{
     using System.Threading;
     using System.Threading.Tasks;
 
-    public interface IItkDispatchRequestRepository {
+    public interface IItkDispatchRequestRepository
+    {
         Task InsertAsync(string id, string request, CancellationToken cancellationToken);
     }
 }
